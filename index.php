6140b5946457de9d25bc19066a5ba227aaf385dc
Connor Vernon <conjamver@hotmail.com>
2019-10-17 22:12:21 +1100

    Review section fixed to loop through all review points for each saver

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
     
     
            //Create SQL string for getting saving account data
-            $sql = "SELECT banks.bank_name, banks.bank_abbr, banks.bank_url, savers.saver_name, savers.saver_date, savers.v_rate, savers.b_rate, savers.req, s_rank.rank, s_rank.rank_color, s_points.point_type, s_points.point_desc FROM ((banks INNER JOIN savers ON banks.bank_id = savers.bank_id) INNER JOIN s_rank ON savers.rank_id = s_rank.rank_id) LEFT JOIN s_points ON savers.saver_id = s_points.s_id WHERE savers.visible = 1 ORDER BY " . $orderByVal;
+            $sql = "SELECT banks.bank_name, banks.bank_abbr, banks.bank_url, savers.saver_id, savers.saver_name, savers.saver_date, savers.v_rate, savers.b_rate, savers.req, s_rank.rank, s_rank.rank_color FROM (banks INNER JOIN savers ON banks.bank_id = savers.bank_id) INNER JOIN s_rank ON savers.rank_id = s_rank.rank_id WHERE savers.visible = 1 ORDER BY " . $orderByVal;
                     
             // Run Query
             $result = mysqli_query($conn, $sql);
@@ -342,37 +342,57 @@
                                     <hr>
                                     
                                     <!--Start of savings Pointss -->
-                                    <div class = "row">
-                                        <!--Pros -->
+                                    <div class="row">
                                         <div class="col-md-12">
-                                            <strong>Strengths
-                                            <br>
-                                            <small>
-                                            <?php 
-                                                if($row["point_type"] == 1){
-                                                    echo '<i class="far fa-smile-beam"></i> ';
-                                                    echo $row["point_desc"];  
-                                                }  
-                                            ?>
-                                                </small>
-                                            </strong>
-                                        </div>
-                                    </div>
-                                    <!--Cons -->
-                                    <div class = "row">
-                                        <div class="col-md-12">
-                                                 <strong>Weakness
-                                            <br>
-                                            <small>
+                                    <strong>Review</strong>
+                                       <?php 
+                                        //if($row["point_type"] == 1){
+                                          //  echo '<i class="far fa-smile-beam"></i> ';
+                                            //echo $row["point_desc"];
+
+                                        //Run new query to show points associated with saving account. Only show points that equal to outer loop saver id. 
+                                        $sql_points = "SELECT savers.saver_id, s_points.s_id, s_points.point_type, s_points.point_desc FROM s_points LEFT JOIN savers ON s_points.s_id = savers.saver_id WHERE savers.saver_id = " . $row['saver_id'] . " ORDER BY point_type ASC";
+
+                                        $result_points = mysqli_query($conn,$sql_points);
+
+                                       //echo mysqli_num_rows($result_points);
+
+                                    if (mysqli_num_rows($result_points) > 0) {
+                                    // output data of each row
+                                        while($row_points = mysqli_fetch_assoc($result_points)) { 
+                                    
+                                            
+                                            if($row_points["point_type"] == 1){?>
                                                 
-                                            <?php 
-                                                if($row["point_type"] == 2){
-                                                    echo '<i class="far fa-frown"></i> ';
-                                                   echo $row["point_desc"];  
-                                                }  
+                                                <div class="s_point_pro">
+                                                <?php
+                                                echo '<i class="far fa-smile-beam"></i> ';
+                                                echo $row_points["point_desc"];
+                                                ?> 
+                                                </div>
+                                                <?php
+                                            }else if($row_points["point_type"] == 2){ ?>
+                                                <div class="s_point_con">
+                                                <?php
+                                                echo '<i class="far fa-frown-open"></i> ';
+                                                echo $row_points["point_desc"];
+                                                ?>
+                                                </div>
+                                                <?php
+                                            }
+                                    
+                                    
+                                    ?>
+                                      
+                                    
+                                          <?php }//END OF while looper for result_points
+
+                                    }else{
+                                        echo "<br>No review.";
+                                    
+                                    }//EMD OF if statement for rows
+
                                             ?>
-                                                </small>
-                                            </strong>
                                         </div>
                                     </div>
                                     
