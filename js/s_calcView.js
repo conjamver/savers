8e587d4c1779f7e9a971323a06f3cb5e9d64f115
Connor Vernon <conjamver@hotmail.com>
2019-10-28 22:48:23 +1100

    Yearly toggle all button update

--- js/s_calcView.js
+++ js/s_calcView.js
@@ -10,7 +10,7 @@ $( document ).ready(function() {
         
 
     
-         
+        //Looper for yearly view 
         for(let i = 0; i < s_btnYear.length; i++){
             s_btnYear[i].addEventListener("click", function(){ 
                 myindex = i;
@@ -70,24 +70,53 @@ $( document ).ready(function() {
         }
     
     
-         
-        //Button that toggles yearly view on all savers visible.
-        for(let i = 0; i < s_btnAllYr.length; i++){
-            s_btnAllYr[i].addEventListener("click", function(){   
-            alert(i);
-            myindex = i;
-
-            //Run looper when we click button.
-            for(let y = 0; y < amtViewInt.length; y++){
-                
-            }
-            
-            
-          });
-            
-        }
     
-    });
+         //Button that toggles yearly view on all savers visible.
+         for (let i = 0; i < s_btnAllYr.length; i++) {
+             s_btnAllYr[i].addEventListener("click", function () {
 
+                 myindex = i;
+                 //Run looper when we click button.
+                 for (let y = 0; y < amtViewInt.length; y++) {
+
+                     //Get buttons in each individual saver item
+                     var s_btn = amtViewInt[y].getElementsByTagName("button");
+                     var s_viewType = amtViewInt[y].getElementsByClassName("s_viewType");
+                     
+                     
+                     //Show/Hide elements
+                    if(s_viewType[1].classList.contains('inactive')) {
+                        s_viewType[0].classList.add("inactive");
+                        s_viewType[1].classList.remove("inactive");
+                    }
+                     
+                     
+                     //loop through all buttons within saver
+                     for (let x = 0; x < s_btn.length; x++) {
+                        
+                         //Remove active class from all month buttons
+                         if(!s_btn[x].classList.contains("s_btnYear")){
+                             s_btn[x].classList.remove("active");
+                        
+                         }
+                         
+                         //Add active class to year button
+                         if(s_btn[x].classList.contains("s_btnYear")){
+                            s_btn[x].classList.add("active");
+                         }
+
+
+                     }//End of button looper
+                     
+                     
+                 }//End of Saver Item Loops
+
+             });
+
+         }//End of Year button looper.
+
+         
+
+}); //End of DOCUMENT GET READY
     
 
