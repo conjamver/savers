cec28a0488d3e9b07fca0edb41433b59bb04af96
Connor Vernon <conjamver@hotmail.com>
2019-11-28 21:26:42 +1100

    Disclaimer and terms update

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,19 @@
 <footer>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-2">
             <img class="img-fluid" src="img/logo.png">
                 <h5>Savers</h5>
-                    <span class="text-muted">Copyright 2019</span>
+                <br>
+                    <span class="text-muted">Copyright <?php echo date("Y"); ?></span>
+                    <a href="terms.php">Terms</a>
+                    <a href="#">Privacy Policy</a>
                 
             </div>
-            <div class="col-md-6 text-right text-muted">
-                
+            <div id="disclaimer" class="col-md-8 text-left text-muted">
+                Disclaimer - Dosh Alley provides general advice on finance and savings accounts which do not take into account your goals and objectives. Please consider this product disclosure before making a financial decision. Information on the website is subject to change and therefore the accuracy of information will be affected. We endeavour to keep information up to date but it is also recommended to confirm information with the actual product. Links throughout the website may direct you to third party websites and we may receive payment for the referrals. If you are confused about your financial objective, please consult a licenced or trusted financial advisor.
+            </div>
+            <div class="col-md-2">
             </div>
         </div>
     </div>
---
+++ terms.php
@@ -0,0 +1,83 @@
+<?php include 'includes/connection.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    
+  
+    <?php include 'includes/header.php'; ?>
+    <?php include 'includes/funcs.php'; ?>
+    
+    
+    
+</head>
+<!-- Inspiration -->
+<!-- https://www.hostgator.com/blog/top-10-most-popular-design-templates-for-gator-website-builder/ -->
+
+<body>
+ <button class="btn btn-sm btn-primary" id="scrollBut" title="Go to top of page"><i class="far fa-arrow-alt-circle-up"></i></button>
+    <?php include 'includes/nav.php'; ?>
+    
+    
+    <div id="main">
+         <!--Welcome Banner --> 
+        <section id="about">
+            <div class="container">
+                <div class="row">
+                    <div class = "col-md-12">
+                        <h1>
+                            Terms and Conditions
+                            <br>
+                            <small>Last updated: 28/11/2019</small>
+                        </h1>
+                        
+                        <p>By using the doshalley.com website and services related to Dosh Alley, you agree to obey the following terms and conditions below.</p>
+                        
+                        <h2>General</h2>
+                        <ol>
+                            <li>The use of Dosh Alley is at the user’s risk and does not aim to influence financial decisions. Dosh Alley is for educational purposes and is not responsible for any financial decisions the user makes.</li>
+                            <li>Dosh Alley provides information in English only and does not support the information in any other languages.</li>
+                            <li>You must not hack, modify or destroy Dosh Alley and its services using malicious content.</li>
+                            <li>You must not transmit any type of malicious content to our website and database such as viruses, worms or any destructive code.</li>
+                            <li>You must not reproduce, duplicate, copy, sell or any portion of Dosh Alley.</li>
+                            <li>Any verbal, physical and written abuse to any users or Dosh Alley members is strictly not allowed. </li>
+                            
+                        </ol>
+                        
+                        <h2>Modification</h2>
+                        <p>Dosh Alley has the rights to make changes to the service and to temporarily or permanently discontinue the service with or without notice.
+
+Terms and conditions are subject to change with or without notice as well as the privacy policy. 
+</p>
+                        <h2>Restictions</h2>
+                        <p>Dosh Alley has the rights to block machines/accounts that may be deemed harmful to the Dosh Alley service.</p>
+                        
+                        <h2>Third-Party Content</h2>
+                        <p>Dosh Alley services may contain links to third-party websites,products or services. These may be affiliates and sponsors. The user of third party content is at your own risk and it is highly recommended to make your own analysis before proceeding with any fiaicial decisions or transaction.</p>
+                        
+                        <h2>Contact us</h2>
+                        <p>Dosh Alley welcomes any comments, questions and concerns. You can contact Dosh Alley via the contact form. </p>
+                        
+                    
+                    </div>
+                    
+
+                </div>
+
+            </div>
+        </section>
+        
+       
+       
+        
+        
+        
+</div>
+  <?php include 'includes/footer.php'; ?>   
+
+</body>
+<script type="application/javascript" src="js/scrollBut.js"></script>
+
+
+    
+</html>
